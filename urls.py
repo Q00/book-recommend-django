f164ffe94f4bf47ee845d8ae5af313b82913b346
Q00 <jqyu.lee@gmail.com>
2019-01-22 19:22:32 +0900

    (feat) Mapping url

--- urls.py
+++ urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import book 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', views.book, name= 'book')
+    path('books/', book, name= 'book')
 ]
